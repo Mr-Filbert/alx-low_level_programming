c22723d6a773a707b85cb008347d72a58888b200
Mr-Filbert <filbertfoli@gmail.com>
2022-04-08 07:45:50 -0700

    Bug fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
+
+/**
+ * main -prints teh phrase "Programming is like building a multilingual puzzle 
+ * Return: 0 if executed properly, non-zero otherwise
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
