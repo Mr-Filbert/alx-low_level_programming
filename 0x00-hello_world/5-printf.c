3b16f64608b431698d07ae503de3aa0a93943061
Mr-Filbert <filbertfoli@gmail.com>
2022-04-08 08:14:08 -0700

    bug fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - prints the phrase with proper grammar, 
- * but the outcome is a piece of art,
- * REturn: 0 if exited properly, non-zero otherwise
+ * main -print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
  */
 int main(void)
 {
