d14940db38ea19c67c6edfc5989cfe73e9819561
Mr-Filbert <filbertfoli@gmail.com>
2022-04-08 08:17:38 -0700

    bug fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 /**
- * main -prints teh phrase "Programming is like building a multilingual puzzle 
- * Return: 0 if executed properly, non-zero otherwise
+ * main -print the string in the put function
+ *
+ * Description: using the main function
+ * this prgram prints "Prgramming is like building a multilingual puzzle
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Prgramming is like building a multilingual puzzle");
 	return (0);
 }
