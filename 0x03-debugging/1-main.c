3531c4e5a025304e5f7d1e5893d73af9259b6adc
Mr-Filbert <filbertfoli@gmail.com>
2022-04-14 08:40:59 -0700

    New file added

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,13 +9,13 @@ int i;
 printf("Infinite loop incoming : (\n");
 i = 0;
 
-/**
+/*
  * while (i < 10)
  * {
+ * infinite loop - No increase of variable
  * putchar(i);
  * }
  */
-
 printf("Infinite loop avaided! \\o/\n");
 return (0);
 }
